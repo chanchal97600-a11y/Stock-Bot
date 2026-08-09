aa23449811b662d8372c2f5a0f85e52c4434d4d7
chanchal97600-a11y <chanchal97600@gmail.com>
2026-08-09 23:19:11 +0530

    Update sheet_runner.py

--- sheet_runner.py
+++ sheet_runner.py
@@ -33,17 +33,20 @@ def send_telegram_message(text):
     except Exception as e:
         print("❌ Telegram error:", e)
 
-# =========================
-# RUN BACKTEST
-# =========================
-print("📊 Running StockSignals...")
-subprocess.run(["python", "StockSignals.py"])
+
 
 # =========================
 # RUN LIVE SCANNER
 # =========================
 print("🚀 Running scanner...")
 subprocess.run(["python", "nse_scanning.py"])
+
+
+# =========================
+# RUN BACKTEST
+# =========================
+print("📊 Running StockSignals...")
+subprocess.run(["python", "StockSignals.py"])
 
 # =========================
 # GOOGLE SHEETS AUTH
